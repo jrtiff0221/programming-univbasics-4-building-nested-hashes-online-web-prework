fcd51db44ea10f5796a6fbe266c38010b461a72f
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-03 21:36:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -68,9 +68,11 @@ def monopoly_with_fourth_tier
 	          "mortgage_value": "$200"
 	        }, 
 	        :b_and_o_railroad => {
-	          "mortgage_value": "$"
+	          "mortgage_value": "$400"
 	        },
-	        :shortline_railroad => {}
+	        :shortline_railroad => {
+	          "mortgage_value": "$800"
+	        }
 	      } 
 	    }
 	}
